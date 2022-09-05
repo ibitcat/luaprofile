96506116be258c6870262f6b260c2a3c948491b7
shui_mu98 <shui_mu98@163.com>
2022-09-05 15:40:03 +0800

    lua profile test.lua

---
+++ test.lua
@@ -0,0 +1,20 @@
+local profile = require "profile"
+profile.start()
+
+local co = coroutine.create(function(value)
+    print("coroutine--->111")
+    coroutine.yield("yield !!!")
+    print("coroutine--->222")
+end)
+
+function foo()
+    local ok, str = coroutine.resume(co,123)
+    print(str)
+    os.execute("sleep 1")
+    coroutine.resume(co,456)
+end
+
+foo()
+-- print(profile.report(10, "cum"))
+-- os.execute("sleep 2")
+print(profile.dstop(10, 4))
